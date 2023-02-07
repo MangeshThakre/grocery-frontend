86097318e236caf2161b3f4f35ac9fffef0ea615
Mangesh Thakre <mangeshthakre933@gmail.com>
2023-02-07 20:49:48 +0530

    updated the appwrite url

--- src/services/appwriteConfig.js
+++ src/services/appwriteConfig.js
@@ -4,7 +4,7 @@ const client = new Client();
 const account = new Account(client);
 
 client
-  .setEndpoint("http://139.59.47.227/v1")
+  .setEndpoint("https://139.59.47.227/v1")
   .setProject("63d97d1a58916ee1295b");
 
 export default account;
